e0089ff55a7c34717e570953d48340a27401137c
bernard-david <dpbernard18@gmail.com>
2021-06-10 20:02:34 -0400

    route created to render the table

--- server.py
+++ server.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask.templating import render_template
 app = Flask(__name__)
+
+@app.route('/')
+def create_table():
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+    return render_template('index.html', users = users)
 
 
 
